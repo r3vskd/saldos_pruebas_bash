00ffe90954cd0f3be09a8378f2fad2ce3500233e
r3vskd <44644302+r3vskd@users.noreply.github.com>
2024-09-01 23:51:20 -0600

    ingresando datos por consola

--- bonk.sh
+++ bonk.sh
@@ -47,8 +47,8 @@ main() {
   }
 
   read -p 'Ingresa el nuemro de cuenta al que deseas depostiar' cuenta
-  read -p 'Deseas depositar o retirar? (ingresa alguuna de las dos palabras: depositar o retirar):  ' cantidad
-  read -p 'Ingresa la cantidad que deseas depositar: '
+  read -p 'Deseas depositar o retirar? (ingresa alguuna de las dos palabras: depositar o retirar):  ' operacion
+  read -p 'Ingresa la cantidad que deseas depositar: ' 
   read -p 'Ingresa el numero de cuenta que deseas visualizar el saldo'
 }
 
